81d97fe8fa6b510873683dc5a4537a8b4d5fcac7
Marcin Kloskowski <kloskowski@gmail.com>
2018-10-21 18:16:34 +0200

    propozycja rozwiązania zadania

--- src/main/java/io/github/cslysy/interview/refactor/Refactor02.java
+++ src/main/java/io/github/cslysy/interview/refactor/Refactor02.java
@@ -1,22 +1,34 @@
 package io.github.cslysy.interview.refactor;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Refactor02 {
 
     private static List<String> namesToUpperCase(final List<List<String>> families) {
         final List<String> result = new ArrayList<>();
-        for (List<String> family : families) {
-            for (String names : family) {
-                if (names != null && !names.isEmpty()) {
-                    result.add(names.toUpperCase());
-                }
-            }
-        }
+
+        families.stream()
+                .flatMap(Collection::stream)
+                .filter(x -> x != null)
+                .filter(x -> x != "")
+                .forEach(x -> result.add(x.toUpperCase()));
+
+
+//        for (List<String> family : families) {
+//            for (String names : family) {
+//                if (names != null && !names.isEmpty()) {
+//                    result.add(names.toUpperCase());
+//                }
+//            }
+//        }
         return result;
     }
 
@@ -26,13 +38,17 @@ public class Refactor02 {
         final List<String> firstFamily = Arrays.asList("john", "", "bob", "danny");
         final List<String> secondFamily = Arrays.asList("james", "peter", null, "laura");
         final List<List<String>> families = Arrays.asList(
-            firstFamily, secondFamily
+                firstFamily, secondFamily
         );
 
         // WHEN
-        // write your code here...
+        List<String> actual = Refactor02.namesToUpperCase(families);
 
         // THEN
-        // write your code here...
+        assertThat(actual).containsExactly("JOHN", "BOB", "DANNY", "JAMES", "PETER", "LAURA");
+
+        //fibonacci, silnia
+        //zozonosc obliczeniaowa Big O notation
+
     }
 }
